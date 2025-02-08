c0c2057f9a401b2ebe7a486f467fb3804705bbf5
BugHunter-X <askzenai@gmail.com>
2025-02-08 01:12:04 +0100

    Add bug.tcl file

---
+++ bug.tcl
@@ -0,0 +1,10 @@
+proc get_element {list index} {
+  if {$index < 0 || $index >= [llength $list]} {
+    error "Index out of range" 
+  }
+  return [lindex $list $index]
+}
+
+set my_list {a b c d e}
+
+puts [get_element $my_list 5]  ;# this will cause an error
